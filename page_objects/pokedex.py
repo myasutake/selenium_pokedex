0771232004f94ebe4bd13e7d172a588ba73cf0fc
Mark Yasutake <mark.yasutake@gmail.com>
2021-02-17 00:19:53 -0800

    Define SearchResult Page Element
    Define methods to return SearchResult's name and number

--- page_objects/pokedex.py
+++ page_objects/pokedex.py
@@ -2,8 +2,9 @@ import logging
 
 from selenium.webdriver.common.by import By
 
-from page_objects.base import BasePage
+from page_objects.base import BaseElement
 from page_objects.base import BaseLoadingElement
+from page_objects.base import BasePage
 
 
 class Page(BasePage):
@@ -12,7 +13,20 @@ class Page(BasePage):
         super().__init__(driver=driver, desc='Pokedex Page', url='https://www.pokemon.com/us/pokedex/')
         self._locators = dict()
         self._locators['loading_indicator'] = (By.CSS_SELECTOR, 'div.loader')
+        self._locators['search_result'] = (By.CSS_SELECTOR, 'li.animating')
         return
+
+    # Search Results
+
+    @property
+    def number_of_results(self):
+        return len(self._find_search_result_objects())
+
+    def _find_search_result_objects(self):
+        elements = self.driver.find_elements(*self._locators['search_result'])
+        return [SearchResult(i) for i in elements]
+
+    # Misc
 
     def accept_cookies(self):
         cookie_modal = CookieModal(driver=self.driver)
@@ -34,6 +48,30 @@ class Page(BasePage):
 
     def is_loaded(self):
         return not self.loading_indicator_is_displayed()
+
+
+class SearchResult(BaseElement):
+
+    def __init__(self, element):
+        super().__init__(element=element, desc='Search Result')
+
+        self._locators = dict()
+        self._locators['name'] = (By.CSS_SELECTOR, 'h5')
+        self._locators['number'] = (By.CSS_SELECTOR, 'p.id')
+
+        self.desc = f"Search Result - {self.number} {self.name}"
+        return
+
+    @property
+    def name(self):
+        return self.element.find_element(*self._locators['name']).text
+
+    @property
+    def number(self):
+        return self.element.find_element(*self._locators['number']).text
+
+    def __str__(self):
+        return self.desc
 
 
 class CookieModal(BaseLoadingElement):
