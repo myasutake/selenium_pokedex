bcb454bb22ec6495b1314d0dd7db4873362d1bce
Mark Yasutake <mark.yasutake@gmail.com>
2021-02-17 23:09:36 -0800

    Change search field tests to load all results after executing a search

--- tests/test_search.py
+++ tests/test_search.py
@@ -13,6 +13,7 @@ def test_search_by_name(load_pokedex_page):
     logging.info("Test begin.")
     driver = load_pokedex_page
     steps.pokedex.execute_search_query(driver=driver, query='th')
+    steps.pokedex.load_all_results(driver=driver)
     steps.pokedex.verify_search_field_results(driver=driver, query='th')
     logging.info("Test passed.")
     return
@@ -22,6 +23,7 @@ def test_search_by_number(load_pokedex_page):
     logging.info("Test begin.")
     driver = load_pokedex_page
     steps.pokedex.execute_search_query(driver=driver, query='20')
+    steps.pokedex.load_all_results(driver=driver)
     steps.pokedex.verify_search_field_results(driver=driver, query='20')
     logging.info("Test passed.")
     return
@@ -32,6 +34,7 @@ def test_search_fail(load_pokedex_page):
     logging.info("Test begin.")
     driver = load_pokedex_page
     steps.pokedex.execute_search_query(driver=driver, query='th')
+    steps.pokedex.load_all_results(driver=driver)
     steps.pokedex.verify_search_field_results(driver=driver, query='ha')
     logging.info("Test passed.")
     return
